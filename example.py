faf5b1e28b8884087497d562b630e5fa9814efdb
Rachmanimir <nicolasmir20@gmail.com>
2021-09-24 14:21:23 +0200

    queries

---
+++ example.py
@@ -0,0 +1,27 @@
+### Imports
+import csv
+import time
+
+### Neo4j imports
+from neo4j import GraphDatabase
+
+uri = input("Neo4j bolt address: ")
+user = input("Neo4j username: ")
+passwd = input('Neo4j password: ')
+
+driver = GraphDatabase.driver(uri, auth=(user, passwd), encrypted=False)
+
+def execute_query(query,driver):
+	with driver.session() as session:
+		session.run(query)
+
+query="MATCH (n) RETURN labels(n)"
+query2="MATCH (n)-[r]-(m) RETURN n,r,m LIMIT 1000"
+l = (query,query2)
+m = (driver,driver)
+
+t1 = time.perf_counter()
+for elt in l:
+	execute_query(elt,driver)
+tf1 = time.perf_counter()
+print("Executing time: ", tf1-t1)
